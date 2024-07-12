5531128026d2d3d43f9a508cc8b84641f8833c70
venedictchen <venedictchen@gmail.com>
2024-07-12 17:32:39 +0700

    add color config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <h1 className="font-heading font-semibold text-5xl ">Yuk kenali</h1>
-      <p className="font-body font-normal text-[#252d8a] text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <main className="flex min-h-screen ">
+      
     </main>
   );
 }
