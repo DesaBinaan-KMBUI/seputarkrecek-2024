addfef46075430b895cb0a7799d68b6e210635e9
venedictchen <venedictchen@gmail.com>
2024-07-12 17:17:03 +0700

    add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import { DM_Sans } from 'next/font/google'
+
 
 const config: Config = {
   content: [
@@ -8,11 +10,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      fontFamily:{
+        heading: ['Clash Display','sans-serif'],
+        body: ['DM Sans','sans-serif']
+      }
     },
   },
   plugins: [],
