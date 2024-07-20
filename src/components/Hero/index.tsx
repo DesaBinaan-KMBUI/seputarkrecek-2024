edd4d1f6c1b6d244f5aaed0956332ed14b775c08
juankhusuma <juan.d.khusuma@gmail.com>
2024-07-20 16:12:10 +0700

    fixed hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,11 +4,11 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const Hero: React.FC = () => {
   const images = [
-    "/images/hero1.png",
-    "/images/hero2.png",
-    "/images/hero3.png",
-    "/images/hero4.png",
-    "/images/hero5.png",
+    "/images/Hero1.png",
+    "/images/Hero2.png",
+    "/images/Hero3.png",
+    "/images/Hero4.png",
+    "/images/Hero5.png",
   ];
 
   const totalImages = images.length;
@@ -21,13 +21,13 @@ const Hero: React.FC = () => {
     if (isTransitioning) return;
     setIsTransitioning(true);
     setTransitionStyle("transform 0.5s ease");
-    if (direction === 'left') {
+    if (direction === "left") {
       setCurrentIndex((prevIndex) => {
-        return ((prevIndex - 1) + extendedImages.length) % extendedImages.length;
+        return (prevIndex - 1 + extendedImages.length) % extendedImages.length;
       });
     } else {
       setCurrentIndex((prevIndex) => {
-        return ((prevIndex + 1) % extendedImages.length);
+        return (prevIndex + 1) % extendedImages.length;
       });
     }
   };
@@ -36,14 +36,13 @@ const Hero: React.FC = () => {
     setIsTransitioning(false);
     if (currentIndex === 0) {
       setCurrentIndex(extendedImages.length - 2);
-      setTransitionStyle('none');
+      setTransitionStyle("none");
       return;
     }
 
     if (currentIndex === extendedImages.length - 1) {
       setCurrentIndex(1);
-      setTransitionStyle('none');
-      return;
+      setTransitionStyle("none");
     }
   };
 
@@ -51,9 +50,8 @@ const Hero: React.FC = () => {
     const interval = setInterval(() => {
       if (!isTransitioning) {
         setTransitionStyle("transform 0.5s ease");
-        setCurrentIndex((prevIndex) => ((prevIndex + 1) % extendedImages.length));
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % extendedImages.length);
       }
-
     }, 5000);
 
     return () => clearInterval(interval);
@@ -82,37 +80,31 @@ const Hero: React.FC = () => {
             </div>
           ))}
         </div>
-
-        {/* Content overlay} */}
-        <div className="absolute inset-0 flex flex-col justify-center items-start p-8 ml-14 mt-8">
-          <div className="relative p-8 w-[550px] rounded-lg max-w-2xl bg-gradient-to-r from-white/30 to-white/10 backdrop-blur-sm border border-white/20 shadow-lg">
-            <h1 className="text-5xl font-bold text-white mb-4 drop-shadow-lg">
+        <div className="absolute top-1/2 -translate-y-1/2 inset-0 flex flex-col justify-center items-start p-8 ml-14 mt-8">
+          <div className="relative px-10 py-4 rounded-lg max-w-2xl bg-gradient-to-r backdrop-blur-sm">
+            <h1 className="text-5xl  font-semibold text-[80px] text-white mb-4 drop-shadow-lg font-clash [text-shadow:_0px_1px_2px_rgba(0,0,0,0.9)]">
               Lorem Ipsum
             </h1>
-            <p className="text-white text-lg mb-6 [text-shadow:_1px_1px_3px_rgba(0,0,0,0.9)] text-justify">
+            <p className="text-gray-200 text-xl tracking-wide font-semibold mb-6 [text-shadow:_1px_1px_3px_rgba(0,0,0,0.9)] text-justify">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
-            <button className="bg-[#84b230] hover:bg-[#658e22] text-white font-semibold py-3 px-6 rounded-md shadow-md transition duration-300 text-lg [text-shadow:_0_1px_1px_rgba(0,0,0,0.2)]">
+            <button className="bg-[#84b230] hover:bg-[#658e22] text-white font-semibold tracking-wide py-3 px-6 rounded-md shadow-md transition duration-300 text-lg [text-shadow:_0_1px_1px_rgba(0,0,0,0.2)]">
               See More
             </button>
           </div>
         </div>
-
-        {/* Previous Button */}
         <button
           className="absolute top-1/2 -translate-y-1/2 left-4 w-10 h-10 ml-3 bg-gray-200 bg-opacity-30 rounded-full flex items-center justify-center border border-black hover:bg-opacity-50 transition-opacity"
-          onClick={() => moveHandler('left')}
+          onClick={() => moveHandler("left")}
         >
           <ChevronLeft />
         </button>
-
-        {/* Next Button */}
         <button
           className="absolute top-1/2 -translate-y-1/2 right-4 w-10 h-10 mr-5 bg-gray-200 bg-opacity-30 rounded-full flex items-center justify-center border border-black hover:bg-opacity-50 transition-opacity"
-          onClick={() => moveHandler('right')}
+          onClick={() => moveHandler("right")}
         >
           <ChevronRight />
         </button>
@@ -122,16 +114,16 @@ const Hero: React.FC = () => {
             <button
               key={index}
               onClick={() => setCurrentIndex(index + 1)}
-              className={`w-2 h-2 rounded-full ${(currentIndex === index + 1) ||
+              className={`w-2 h-2 rounded-full ${
+                currentIndex === index + 1 ||
                 (currentIndex === 0 && index === images.length - 1) ||
                 (currentIndex === extendedImages.length - 1 && index === 0)
-                ? "bg-white scale-150"
-                : "bg-gray-500"
-                } transition-transform duration-300`}
+                  ? "bg-white scale-150"
+                  : "bg-gray-500"
+              } transition-transform duration-300`}
             />
           ))}
         </div>
-
       </div>
     </div>
   );
---
+++ src/components/Testimoni/card.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface TestimoniCardProps {
+  imageSrc: string;
+  rating: number;
+  name: string;
+  institution: string;
+  testimonial: string;
+}
+
+
+const TestimoniCard: React.FC<TestimoniCardProps> = ({ imageSrc, rating, name, institution, testimonial }) => {
+  return (
+    <div className="relative w-full h-60">
+      <Image
+        src={imageSrc}
+        alt={name}
+        layout="fill"
+        objectFit="cover"
+        className="w-full h-full"
+      />
+      <div className="absolute right-0 top-0 ml-[250px] h-60 overflow-auto flex flex-col text-white p-6 bg-black/50 shadow-lg">
+        <div className="flex flex-row gap-1 justify-end mb-1 md:mb-4">
+          <Image
+            src="/images/star.svg"
+            alt={name}
+            objectFit="cover"
+            height={13}
+            width={13}
+          />
+          <p className="text-xs sm:text-sm font-semibold">{rating}</p>
+        </div>
+
+        <h1 className="text-base sm:text-xl font-bold mb-2">{name}</h1>
+        <p className="text-xs sm:text-sm font-bold mb-1">{institution}</p>
+        <p className="text-xs sm:text-sm">
+          &quot;{testimonial}&quot;
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TestimoniCard;
