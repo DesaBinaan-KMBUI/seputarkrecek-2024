4c0c7a91f301a7da331b0a08b6414c61d9d2c550
venedictchen <venedictchen@gmail.com>
2024-07-22 16:43:25 +0700

    feat:section kopi and umkm skeleton card

--- src/pages/produk-pangan/index.tsx
+++ src/pages/produk-pangan/index.tsx
@@ -12,28 +12,28 @@ const Carousel = dynamic(() => import("@/components/Carousel3D"), {
 const ProdukPangan = () => {
     const [goToSlide, setGoToSlide] = useState(0);
     const images = ["/images/hero1.png"];
-
+    const items = [
+        { "name": "Gamelan", "image": "/images/Hero2.png" },
+        { "name": "Gamelan", "image": "/images/Hero2.png" },
+        { "name": "Gamelan", "image": "/images/Hero2.png" },
+        { "name": "Gamelan", "image": "/images/Hero2.png" }
+    ]
     const totalSlides = 3;
-    const cardEachSide = totalSlides / 2;
-
-    const colors = [
-        "#40571d",
-        "#a2cd4e",
-        "#a2cd4e",
-    ];
+    const cardEachSide = Math.floor(totalSlides / 2);
+    console.log("cardEachSide", cardEachSide);
 
     const calculateRotation = (position: number) => {
-        const distance = ((position-goToSlide)+totalSlides)%totalSlides;
+        const distance = ((position - goToSlide) + totalSlides) % totalSlides;
 
         if (position === goToSlide) return 0;
-        return distance >= cardEachSide ? -25 : 25;
+        return distance > cardEachSide ? -25 : 25;
     };
 
 
     const slides = Array.from({ length: totalSlides }, (_, index) => ({
         key: index,
         content: <CarouselCard
-            first={index===goToSlide}
+            first={index === goToSlide}
             rotation={calculateRotation(index)}
         />,
     }));
@@ -56,7 +56,7 @@ const ProdukPangan = () => {
 
     return (
         <div className="flex min-h-screen flex-col items-center">
-            <div className="relative w-screen h-screen overflow-hidden">
+            <section className="relative w-screen h-screen overflow-hidden">
                 <div className="relative w-full h-full overflow-hidden">
                     <div className="slide w-full h-full flex">
                         <div className="w-full h-full flex-shrink-0 relative">
@@ -87,8 +87,9 @@ const ProdukPangan = () => {
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex flex-col gap-y-52  min-h-5xl my-16 justify-between items-center text-center">
+            </section>
+
+            <section className="flex flex-col gap-y-52  min-h-5xl my-16 justify-between items-center text-center">
                 <div className="mb-28">
                     <h1 className="text-[80px] font-clash text-deep-cove-900 font-semibold">Komoditas Pangan</h1>
                     <h1 className="text-[80px] font-clash text-deep-cove-900 font-semibold">Dusun Krecek</h1>
@@ -102,15 +103,15 @@ const ProdukPangan = () => {
                         <ChevronLeft />
                     </button>
                     <div className="w-3/4 ">
-                    <Carousel
-                        slides={slides}
-                        goToSlide={goToSlide}
-                        offsetRadius={2}
+                        <Carousel
+                            slides={slides}
+                            goToSlide={goToSlide}
+                            offsetRadius={2}
 
-                        showNavigation={false}
-                        animationConfig={config.gentle}
-                        goToSlideDelay={100}
-                    />
+                            showNavigation={false}
+                            animationConfig={config.gentle}
+                            goToSlideDelay={100}
+                        />
 
                     </div>
 
@@ -126,7 +127,67 @@ const ProdukPangan = () => {
                     <h1 className="w-[66%] text-[24px] font-dm text-deep-cove-900 font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h1>
 
                 </div>
-            </div>
+            </section>
+
+            <section className="grid grid-cols-2 mt-10 font-clash w-full h-full">
+                <div className="text-lg px-10 py-20 bg-gradient-to-l from-lime-500 to-lime-700 text-white text-left">
+
+                    LOREM IPSUM
+
+                    <h1 className="my-3 font-bold text-5xl">
+                        Kopi Kinasih
+                    </h1>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.
+                </div>
+                <div className="bg-gradient-to-tr from-lime-500 via-lime-300 to-lime-100">
+                    <img
+                        className="h-full w-full object-cover"
+                        src="/images/Hero3.png"
+                        alt="kopi" />
+                </div>
+            </section>
+
+            <section className="flex flex-col my-16 w-full items-center text-center">
+                <div className="mb-28">
+                    <h1 className="text-[80px] font-clash text-deep-cove-900 font-semibold">UMKM Dusun Krecek</h1>
+                </div>
+
+                <div className="flex w-full   justify-center flex-col items-center" >
+                    <div className="flex flex-col w-[280px]  gradient-border  rounded-xl  bg-white">
+                        <div className="flex gap-4 py-8 px-8 w-full flex-col">
+                            <div className="relative w-full h-[200px]">
+                                <Image
+                                    src="/images/Hero3.png"
+                                    alt="Hero Image"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    priority
+                                />
+                            </div>
+
+
+                            <div className="font-clash text-deep-cove-900 font-semibold text-left">
+                                <h1 className="leading-[30px]">Keripik Kentang</h1>
+                                <h1 className="leading-[30px]">Rp 50000</h1>
+                            </div>
+
+
+                            <button className="bg-conifer-600 px-10 py-2 text-white font-semibold font-dm rounded-xl">
+                                Beli
+                            </button>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+            </section>
+
         </div>
     );
 };
